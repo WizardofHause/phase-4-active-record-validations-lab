f6664d46d9bdf4ea70b074f4f08665a4ed183336
WizardofHause <jonrhause@gmail.com>
2022-11-28 12:02:24 -0700

    commit tree de1432684331bf2ee882150de2c5abcf69d73216

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+    validate :make_clickbaity
+
+    CLICKBAIT = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+
+    def make_clickbaity
+        if CLICKBAIT.none? { |bait| bait.match title}
+            errors.add(:title, "Not clickbait-y enough!")
+        end
+    end
+
 end
